cea7a7c013ad2da804e9e1760dfbf5e7759e0503
Devprodipraj <devprodipraj@gmail.com>
2025-03-05 21:24:31 +0600

    add some update

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let cursor = document.getElementById("CustomCursor");
 
-document.addEventListener("mousemove", function(e) {
+document.addEventListener("mousemove", function(dest) {
     gsap.to(cursor, {
-        x: e.clientX,
-        y: e.clientY,
+        x: dest.x,
+        y: dest.y,
         duration: 0.2,
         ease: "power2.out"
     });
+    
 });
